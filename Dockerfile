a5298035700dcd051351c307f45c040402779dca
Jones-Omoyibo <Jonesbenklins@gmail.com>
2025-03-17 13:28:25 -0500

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,21 @@
-#FROM eclipse-temurin:23-jdk-alpine AS builder
-#LABEL authors="jones"
-#
-#WORKDIR /build
-#
-#COPY mvnw pom.xml ./
-#
-#COPY .mvn .mvn
-#
-#COPY src src
-#
-#RUN chmod +x mvnw && ./mvnw clean package -DskipTests
-#
-## Stage 2: Create a lightweight runtime image
-#FROM eclipse-temurin:23-jre-alpine
-#WORKDIR /app
-#
-## Copy the built JAR from the builder stage
-#COPY --from=builder /build/target/api-gateway-0.0.1-SNAPSHOT.jar app.jar
-#
-#
-#ENTRYPOINT ["java", "-jar", "/app/app.jar"]
+FROM eclipse-temurin:23-jdk-alpine AS builder
+LABEL authors="jones"
+
+WORKDIR /build
+
+COPY mvnw pom.xml ./
+
+COPY .mvn .mvn
+
+COPY src src
+
+RUN chmod +x mvnw && ./mvnw clean package -DskipTests
+
+
+FROM eclipse-temurin:23-jre-alpine
+WORKDIR /app
+
+COPY --from=builder /build/target/api-gateway-0.0.1-SNAPSHOT.jar api-gateway.jar
+
+
+ENTRYPOINT ["java", "-jar", "/app/api-gateway.jar"]
--- src/main/java/com/movienetscape/apigateway/filter/ApiGatewayFilter.java
+++ src/main/java/com/movienetscape/apigateway/filter/ApiGatewayFilter.java
@@ -4,10 +4,25 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
+
+import java.util.Set;
 
 @Component
 public class ApiGatewayFilter extends AbstractGatewayFilterFactory<ApiGatewayFilter.Config> {
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+    private static final Set<String> UNPROTECTED_PATHS = Set.of(
+            "/api/v1/auth/login",
+            "/api/v1/auth/create",
+            "/api/v1/auth/forgot-password/**",
+            "/api/v1/auth/change-password",
+            "/api/v1/auth/verify-password-reset-token",
+            "/api/v1/users/register",
+            "/api/v1/users/verify"
+
+    );
 
     public ApiGatewayFilter() {
         super(Config.class);
@@ -16,20 +31,25 @@ public class ApiGatewayFilter extends AbstractGatewayFilterFactory<ApiGatewayFil
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
+            String path = exchange.getRequest().getURI().getPath();
+
+
+            boolean isUnprotected = UNPROTECTED_PATHS.stream()
+                    .anyMatch(pattern ->  pathMatcher.match(pattern, path));
+
+            if (isUnprotected) {
+                return chain.filter(exchange);
+            }
             String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-            System.out.println("authHeader: " + authHeader);
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
-            System.out.println("authHeader: " + authHeader);
+
             return chain.filter(exchange);
         };
     }
 
-
     public static class Config {
     }
 }
-
-
